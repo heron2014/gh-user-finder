1e71245354494ca57ab49f6caa1959c3cae0afe4
Anita <a.nita@hotmail.co.uk>
2016-04-24 20:39:55 +0100

    created Profile component and issue with router see https://github.com/heron2014/gh-user-finder/issues/2#issuecomment-214025498

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 const root = document.querySelector('.root');
 
 render((
   <Router history={hashHistory}>
     <Route path='/' component={App}>
+      <Route path='/profile/:username' component={Profile} />
       <IndexRoute component={Home} />
     </Route>
   </Router>
